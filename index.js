3c6f7bc3e495450f12f76d9e28d2fdfbe6467405
priyanshu <priyanshu07g@gmail.com>
2024-10-27 13:35:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+
+
+app.post("/user/signup" , async function(req, res){
+
+})
+
+app.post("/user/signin" , async function(req, res){
+    
+})
+
+
+app.get("user/purchases" , async function(req, res){
+    
+})
+
+
+app.get("/course/purchase" , function(req, res){
+    
+})
+
+
+app.get("/course" , function(req, res){
+    
+})
+
+
+
+app.post("/admin-signup" , function(req, res){
+    
+})
+
+
+app.post("/admin-signin" , function(req, res){
+    
+})
+
+app.post("/create" , function(req, res){
+    
+})
+
+
+app.delete("/delete" , function(req,res){
+
+})
+
+app.listen(3000);
